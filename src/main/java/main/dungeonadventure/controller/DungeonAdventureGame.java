544471798a6ac8888b3f6583908dfa39247b5495
IAmMeers <smithl64@uw.edu>
2022-07-27 20:24:46 -0700

    Modified DunegonAdventureGUI to be launched from DungeonAdventureGame

---
+++ src/main/java/main/dungeonadventure/controller/DungeonAdventureGame.java
@@ -0,0 +1,32 @@
+package main.dungeonadventure.controller;
+
+
+
+import main.dungeonadventure.model.Dungeon;
+//import main.dungeonadventure.view.DungeonAdventureGUI;
+
+public class DungeonAdventure {
+
+
+//    private static Dungeon myDungeon;
+//    private static DungeonAdventureGUI myGui;
+
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+        //Generate a Dungeon
+        //myDungeon = new Dungeon();
+
+        //Generate a Dungeon GUI object for commanding the GUI
+       // myGui = new DungeonAdventureGUI(myDungeon);
+
+        //DungeonAdventureGUI.launch();
+
+
+    }
+
+
+
+
+
+}
