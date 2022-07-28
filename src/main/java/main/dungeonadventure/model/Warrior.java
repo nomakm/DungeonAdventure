dc88d2a86cb6d8bfa6efb9b94a8720305bf84168
IAmMeers <smithl64@uw.edu>
2022-07-27 20:26:07 -0700

    Modified pom for main method issue

--- src/main/java/main/dungeonadventure/model/Warrior.java
+++ src/main/java/main/dungeonadventure/model/Warrior.java
@@ -1,4 +1,5 @@
 package main.dungeonadventure.model;
 
 public class Warrior {
+
 }
--- src/main/java/main/dungeonadventure/view/DungeonAdventureGUI.java
+++ src/main/java/main/dungeonadventure/view/DungeonAdventureGUI.java
@@ -5,12 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.dungeonadventure.model.Dungeon;
 
 import java.io.IOException;
 
 public class DungeonAdventureGUI extends Application {
+
+    private Dungeon myDungeon;
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(final Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("welcome_screen.fxml"));
         Scene scene = new Scene(root);
         String css = this.getClass().getResource("stylesheet.css").toExternalForm();
@@ -20,7 +24,20 @@ public class DungeonAdventureGUI extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    /**
+     * Used to launch GUI from main controller class DungeonAdventureGame
+     */
+    public void launchGUI() {
         launch();
+    }
+
+    /**
+     * Adds a dungeon object and assigns to myDungeon.
+     * Can't create a constructor to handle this due to class
+     * extending Application.
+     * @param theDungeon Input Dungeon
+     */
+    public void addDungeon(final Dungeon theDungeon) {
+        myDungeon = theDungeon;
     }
 }
