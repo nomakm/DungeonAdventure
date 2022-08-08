6af6ebd4b9ab302830cbdf690d49a15576cf6006
Masayoki <Masayoki88@yahoo.com>
2022-08-08 14:22:31 -0700

    Testing Upload/Fixing Wording Errior

--- src/main/java/main/dungeonadventure/model/Thief.java
+++ src/main/java/main/dungeonadventure/model/Thief.java
@@ -1,17 +1,17 @@
 package main.dungeonadventure.model;
 
 public class Thief extends Hero {
-    private final int mySurpiseAtk;
+    private final int mySurpriseAtk;
     private final int myCaught;
 
     public Thief(final String theCharacterName) {
         super(theCharacterName, 75, 20, 40, 6, 8, 4);
-        this.mySurpiseAtk = 4;
+        this.mySurpriseAtk = 4;
         this.myCaught = 2;
     }
 
-    public int getMySurpiseAtk() {
-        return mySurpiseAtk;
+    public int getMySurpriseAtk() {
+        return mySurpriseAtk;
     }
 
     public int getMyCaught() {
