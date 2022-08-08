34a81082eb4a1ba0a3b445a3f6ae830febe11076
Masayoki <Masayoki88@yahoo.com>
2022-08-08 14:27:42 -0700

    Testing Upload/Fixing Wording Error

--- src/main/java/main/dungeonadventure/model/Hero.java
+++ src/main/java/main/dungeonadventure/model/Hero.java
@@ -3,16 +3,16 @@ package main.dungeonadventure.model;
 public class Hero extends DungeonCharacter {
 
     private final String myCharName;
-    private final int myChancetoBlock;
+    private final int myBlockChance;
 
     public Hero (final String theCharacterName, final int theHP,
                  final int theDmgMin, final int theDmgMax,
                  final int theAtkSpd, final int theHitRate,
-                 int theChancetoBlock) {
+                 int theBlockChance) {
 
         super(theHP, theDmgMin, theDmgMax, theAtkSpd, theHitRate);
         this.myCharName = theCharacterName;
-        this.myChancetoBlock = theChancetoBlock;
+        this.myBlockChance = theBlockChance;
     }
 
     public String getMyCharacterName() {
@@ -20,7 +20,7 @@ public class Hero extends DungeonCharacter {
     }
 
     public int getMyChanceToBlock() {
-        return myChancetoBlock;
+        return myBlockChance;
     }
 
 }
