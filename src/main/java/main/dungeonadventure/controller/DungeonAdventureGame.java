0c71de05477539aa7178f45dc8b02648bbc6e3bd
IAmMeers <smithtech253@gmail.com>
2022-08-08 18:25:14 -0700

    Added save game and load game functions (Not done)

--- src/main/java/main/dungeonadventure/controller/DungeonAdventureGame.java
+++ src/main/java/main/dungeonadventure/controller/DungeonAdventureGame.java
@@ -5,6 +5,7 @@ import main.dungeonadventure.model.*;
 import main.dungeonadventure.view.DungeonAdventureGUI;
 
 import java.awt.*;
+import java.io.*;
 
 /**
  * Main class used for launching game GUI and handling communication between GUI
@@ -31,24 +32,56 @@ public class DungeonAdventureGame {
 
     }
 
-    //moved to testRoomController
-    public void moveHero(final Direction theDirection) {
-        Point currentPos = myDungeon.getHeroPosition();
-        if (theDirection == Direction.NORTH) {
-            myDungeon.setHeroPosition(currentPos.x - 1, currentPos.y);
-        } else if (theDirection == Direction.WEST) {
-            myDungeon.setHeroPosition(currentPos.x, currentPos.y - 1);
-        } else if (theDirection == Direction.EAST) {
-            myDungeon.setHeroPosition(currentPos.x, currentPos.y + 1);
-        } else if (theDirection == Direction.SOUTH) {
-            myDungeon.setHeroPosition(currentPos.x + 1, currentPos.y);
+    public void saveGame(String theFilePath) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("/saves/game.ser");
+            //FileOutputStream fileOut = new FileOutputStream(theFilePath);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(myDungeon);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in /saves/game.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public void loadGame(String theFilePath) {
+        try {
+            FileInputStream fileIn = new FileInputStream("/saves/game.ser");
+            //FileInputStream fileIn = new FileInputStream(theFilePath);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            myDungeon = (Dungeon) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Dungeon class not found");
+            c.printStackTrace();
+            return;
         }
     }
 
     //moved to testRoomController
-    public Room getHeroRoom() {
-        return myDungeon.getCurrentRoom();
-    }
+//    public void moveHero(final Direction theDirection) {
+//        Point currentPos = myDungeon.getHeroPosition();
+//        if (theDirection == Direction.NORTH) {
+//            myDungeon.setHeroPosition(currentPos.x - 1, currentPos.y);
+//        } else if (theDirection == Direction.WEST) {
+//            myDungeon.setHeroPosition(currentPos.x, currentPos.y - 1);
+//        } else if (theDirection == Direction.EAST) {
+//            myDungeon.setHeroPosition(currentPos.x, currentPos.y + 1);
+//        } else if (theDirection == Direction.SOUTH) {
+//            myDungeon.setHeroPosition(currentPos.x + 1, currentPos.y);
+//        }
+//    }
+
+    //moved to testRoomController
+//    public Room getHeroRoom() {
+//        return myDungeon.getCurrentRoom();
+//    }
 
     //moved to ChooseHeroController
 //    public void createHero(final HeroType theHeroType,
