1f24733b9e33e504dae8eba5a4a616ac70a77194
IAmMeers <smithl64@uw.edu>
2022-08-15 14:32:48 -0700

    Documentation

--- src/main/java/main/dungeonadventure/model/Direction.java
+++ src/main/java/main/dungeonadventure/model/Direction.java
@@ -1,5 +1,8 @@
 package main.dungeonadventure.model;
 
+/**
+ * Enum used identifying a direction.
+ */
 public enum Direction {
     NORTH,
     SOUTH,
--- src/main/java/main/dungeonadventure/model/Dungeon.java
+++ src/main/java/main/dungeonadventure/model/Dungeon.java
@@ -8,9 +8,11 @@ import java.util.NoSuchElementException;
 import java.util.Random;
 
 /**
- * Dungeon class used to hold maze of game
+ * Dungeon class used to hold overall game data. Holds dungeon maze
+ * comprised of rooms and the position of the hero in the maze.
  *
  * @author Luke Smith
+ * @Author Micaela Nomakchteinsky
  * @version 8-6-22
  */
 public class Dungeon implements Serializable {
@@ -26,14 +28,15 @@ public class Dungeon implements Serializable {
     /** Total pillars on map*/
     private static final int PILLAR_COUNT = 4;
 
-    /** 2D array for storing rooms. */
+    /** 2D array for storing rooms. Represents Dungeon map. */
     private Room[][] myRooms;
-    /** */
+    /** Position of hero in maze. */
     Point myHeroPosition;
-    /** Position of entrance*/
+    /** Position of entrance. */
     Point myEntranceLocation;
-
+    /** Player's hero character used in game. */
     private Hero myHero;
+
 
     /**
      * Constructs a solvable Dungeon map.
@@ -50,8 +53,9 @@ public class Dungeon implements Serializable {
 
     }
 
+
     /**
-     *
+     * Builds a dungeon step by step.
      */
     private void buildDungeon() {
         generateRooms();
@@ -62,21 +66,10 @@ public class Dungeon implements Serializable {
         setHeroPosition(myEntranceLocation.x, myEntranceLocation.y);
     }
 
-    private void spawnPillars() {
-        int spawnedPillars = 0;
-        while (spawnedPillars < PILLAR_COUNT) {
-            int x = RAND_GEN.nextInt(DUNGEON_HEIGHT);
-            int y = RAND_GEN.nextInt(DUNGEON_WIDTH);
 
-            if (!myRooms[x + 1][y + 1].containsPillar()) {
-                myRooms[x + 1][y + 1].addPillar();
-                spawnedPillars++;
-            }
-
-        }
-
-    }
-
+    /**
+     * Generates a room object in each position in the array.
+     */
     private void generateRooms() {
         for (int row = 1; row < myRooms.length - 1; row++) {
             for (int col = 1; col < myRooms[0].length - 1; col++) {
@@ -85,12 +78,24 @@ public class Dungeon implements Serializable {
         }
     }
 
+
+    /**
+     * Places entrances and exits in specific rooms.
+     */
     private void generateEntrancesAndExits() {
         //Set Entrance and Exit points of maze
         myRooms[1][1].setEntrance(true);
         myRooms[DUNGEON_HEIGHT][DUNGEON_WIDTH].setExit(true);
     }
 
+
+    /**
+     * Randomly places doors in rooms. Will not place a door leading
+     * outside the dungeon space. A door placed in one direction will
+     * have the adjacent room the door connects to also be open.
+     * Ex: A room's north door is open, the room above it will have
+     * it's south door open in return.
+     */
     private void generateDoorsInRooms() {
         int chanceToSpawnDoor = 50;
         for (int row = 1; row < myRooms.length - 1; row++) {
@@ -126,6 +131,167 @@ public class Dungeon implements Serializable {
     }
 
 
+    /**
+     * Randomly spawns pillars in each room.
+     */
+    private void spawnPillars() {
+        int spawnedPillars = 0;
+        while (spawnedPillars < PILLAR_COUNT) {
+            int x = RAND_GEN.nextInt(DUNGEON_HEIGHT);
+            int y = RAND_GEN.nextInt(DUNGEON_WIDTH);
+
+            if (!myRooms[x + 1][y + 1].containsPillar()) {
+                myRooms[x + 1][y + 1].addPillar();
+                spawnedPillars++;
+            }
+
+        }
+
+    }
+
+
+    /**
+     * Sets the location of the room that is the entrance of the
+     * dungeon.
+     *
+     * @param theLocation Location of room.
+     */
+    private void setEntrance(Point theLocation) {
+        //Check for out of bounds
+        if (theLocation.x < 1 || theLocation.x > DUNGEON_WIDTH
+            && theLocation.y < 1 || theLocation.y > DUNGEON_HEIGHT) {
+            throw new IllegalArgumentException("Position of entrance is out of bounds");
+        }
+        myEntranceLocation = theLocation;
+
+    }
+
+
+    /**
+     * Searches through dungeon for entrance.
+     */
+    private void findEntrance() {
+        Point entrance = null;
+        for (int row = 1; row < myRooms.length - 1; row++) {
+            for (int col = 1; col < myRooms[0].length - 1; col++) {
+                if (myRooms[row][col].isEntrance()) {
+                    entrance = new Point(row, col);
+                    setEntrance(entrance);
+                    break;
+                }
+            }
+        }
+        if (entrance == null) {
+            throw new NoSuchElementException("Entrance could not be found");
+        }
+    }
+
+
+    /**
+     * Returns position of hero in dungeon.
+     * @return position of hero.
+     */
+    public Point getHeroPosition(){
+        return myHeroPosition;
+    }
+
+
+    /**
+     * Gets a random roll used for game RNG system.
+     * @return random value from 0 -> UPPERBOUND
+     */
+    public static int getRandomRoll() {
+        return RAND_GEN.nextInt(RAND_UPPERBOUND);
+    }
+
+
+    /**
+     * Set's position of hero in the Dungeon.
+     * @param theX value of room location, North-South axis.
+     * @param theY value of room location, West-East axis.
+     */
+    public void setHeroPosition(final int theX, final int theY) {
+        Point location = new Point(theX, theY);
+        //Check for out of bounds
+        if (location.x < 1 || location.x > DUNGEON_WIDTH
+                && location.y < 1 || location.y > DUNGEON_HEIGHT) {
+            throw new IllegalArgumentException("Position of entrance is out of bounds");
+        }
+        myHeroPosition = location;
+    }
+
+
+    /**
+     * Set's hero object for dungeon.
+     * @param theHero Hero object.
+     */
+    public void setHero(Hero theHero) {
+        if (theHero == null) {
+            throw new IllegalArgumentException("Hero is null");
+        } else {
+            myHero = theHero;
+        }
+    }
+
+
+    /**
+     * Returns hero object used in game.
+     * @return hero object used in game.
+     */
+    public Hero getHero() {
+        return myHero;
+    }
+
+
+    /**
+     * Return 2d array of rooms
+     * @return 2d array of rooms
+     */
+    public Room[][] getRooms() {
+        return myRooms;
+    }
+
+
+    /**
+     * Returns how many pillars should be in the dungeon.
+     * @return  how many pillars should be in the dungeon.
+     */
+    public int getPillarCount() {
+        return PILLAR_COUNT;
+    }
+
+
+    /**
+     * Returns the room object the Hero is located in.
+     * @return Returns the room object the Hero is located in.
+     */
+    public Room getCurrentRoom() {
+        return myRooms[myHeroPosition.x][myHeroPosition.y];
+    }
+
+
+    /**
+     * Returns surrounding rooms of Hero's current room.
+     * @return
+     */
+    public HashMap<String, Room> getNeighbors() {
+        HashMap<String, Room> neighbors = new HashMap<>();
+        neighbors.put("NORTHWEST",myRooms[myHeroPosition.x - 1][myHeroPosition.y - 1]);
+        neighbors.put("NORTH",myRooms[myHeroPosition.x - 1][myHeroPosition.y]);
+        neighbors.put("NORTHEAST",myRooms[myHeroPosition.x - 1][myHeroPosition.y + 1]);
+
+        neighbors.put("WEST",myRooms[myHeroPosition.x][myHeroPosition.y - 1]);
+        neighbors.put("CURR",myRooms[myHeroPosition.x][myHeroPosition.y]);
+        neighbors.put("EAST",myRooms[myHeroPosition.x][myHeroPosition.y + 1]);
+
+        neighbors.put("SOUTHWEST",myRooms[myHeroPosition.x + 1][myHeroPosition.y - 1]);
+        neighbors.put("SOUTH",myRooms[myHeroPosition.x + 1][myHeroPosition.y]);
+        neighbors.put("SOUTHEAST", myRooms[myHeroPosition.x + 1][myHeroPosition.y + 1]);
+
+        return neighbors;
+    }
+
+
 
     /**
      * Used to visualize door placements in Dungeon (Testing)
@@ -145,95 +311,6 @@ public class Dungeon implements Serializable {
         }
 
         return dungeonMap.toString();
-    }
-
-    private void setEntrance(Point theLocation) {
-        //Check for out of bounds
-        if (theLocation.x < 1 || theLocation.x > DUNGEON_WIDTH
-            && theLocation.y < 1 || theLocation.y > DUNGEON_HEIGHT) {
-            throw new IllegalArgumentException("Position of entrance is out of bounds");
-        }
-        myEntranceLocation = theLocation;
-
-    }
-
-    private void findEntrance() {
-        Point entrance = null;
-        for (int row = 1; row < myRooms.length - 1; row++) {
-            for (int col = 1; col < myRooms[0].length - 1; col++) {
-                if (myRooms[row][col].isEntrance()) {
-                    entrance = new Point(row, col);
-                    setEntrance(entrance);
-                    break;
-                }
-            }
-        }
-        if (entrance == null) {
-            throw new NoSuchElementException("Entrance could not be found");
-        }
-    }
-
-    public Point getHeroPosition(){
-        return myHeroPosition;
-    }
-
-    public static int getRandomRoll() {
-        return RAND_GEN.nextInt(RAND_UPPERBOUND);
-    }
-
-    public void setHeroPosition(final int theX, final int theY) {
-        Point location = new Point(theX, theY);
-        //Check for out of bounds
-        if (location.x < 1 || location.x > DUNGEON_WIDTH
-                && location.y < 1 || location.y > DUNGEON_HEIGHT) {
-            throw new IllegalArgumentException("Position of entrance is out of bounds");
-        }
-        myHeroPosition = location;
-    }
-
-    public void setHero(Hero theHero) {
-        if (theHero == null) {
-            throw new IllegalArgumentException("Hero is null");
-        } else {
-            myHero = theHero;
-        }
-    }
-
-    public Hero getHero() {
-        return myHero;
-    }
-
-    /**
-     * Return 2d array of rooms
-     * @return
-     */
-    public Room[][] getRooms() {
-        return myRooms;
-    }
-
-    public int getPillarCount() {
-        return PILLAR_COUNT;
-    }
-
-    public Room getCurrentRoom() {
-        return myRooms[myHeroPosition.x][myHeroPosition.y];
-    }
-
-    public HashMap<String, Room> getNeighbors() {
-        HashMap<String, Room> neighbors = new HashMap<>();
-        neighbors.put("NORTHWEST",myRooms[myHeroPosition.x - 1][myHeroPosition.y - 1]);
-        neighbors.put("NORTH",myRooms[myHeroPosition.x - 1][myHeroPosition.y]);
-        neighbors.put("NORTHEAST",myRooms[myHeroPosition.x - 1][myHeroPosition.y + 1]);
-
-        neighbors.put("WEST",myRooms[myHeroPosition.x][myHeroPosition.y - 1]);
-        neighbors.put("CURR",myRooms[myHeroPosition.x][myHeroPosition.y]);
-        neighbors.put("EAST",myRooms[myHeroPosition.x][myHeroPosition.y + 1]);
-
-        neighbors.put("SOUTHWEST",myRooms[myHeroPosition.x + 1][myHeroPosition.y - 1]);
-        neighbors.put("SOUTH",myRooms[myHeroPosition.x + 1][myHeroPosition.y]);
-        neighbors.put("SOUTHEAST", myRooms[myHeroPosition.x + 1][myHeroPosition.y + 1]);
-
-        return neighbors;
     }
 
 }
